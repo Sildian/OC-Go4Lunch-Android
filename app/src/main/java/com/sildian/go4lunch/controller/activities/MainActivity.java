00cb1fa512bb66d9b7d5402c5bb5f44d3a029154
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2019-07-19 17:58:45 +0200

    Improved authentification, defined styles and added a toolbar within 'MainActivity'

--- app/src/main/java/com/sildian/go4lunch/controller/activities/MainActivity.java
+++ app/src/main/java/com/sildian/go4lunch/controller/activities/MainActivity.java
@@ -1,35 +1,100 @@
 package com.sildian.go4lunch.controller.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.sildian.go4lunch.R;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int KEY_REQUEST_LOGIN=10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO improve this
-        startSignInActivity();
+        setSupportActionBar((Toolbar)findViewById(R.id.activity_main_toolbar));
+        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+        if(user==null) {
+            startLoginActivity();
+        }
     }
 
-    //TODO improve this
-    private void startSignInActivity(){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_toolbar_search:
+                //TODO replace by an action
+                Log.i("TAG_MENU", "Search");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case KEY_REQUEST_LOGIN:
+                handleLoginResult(resultCode, data);
+                break;
+        }
+    }
+
+    private void startLoginActivity(){
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
-                        //.setTheme(R.style.LoginTheme)
+                        .setTheme(R.style.LoginTheme)
                         .setAvailableProviders(
                                 Arrays.asList(new AuthUI.IdpConfig.FacebookBuilder().build(),
                                         new AuthUI.IdpConfig.GoogleBuilder().build()))
                         .setIsSmartLockEnabled(false, true)
-                        //.setLogo(R.drawable.ic_logo_auth)
+                        .setLogo(R.drawable.ic_go4lunch_main)
                         .build(),
-                100);
+                KEY_REQUEST_LOGIN);
+    }
+
+    private void handleLoginResult(int resultCode, Intent data) {
+
+        IdpResponse loginResponse = IdpResponse.fromResultIntent(data);
+
+        //TODO add actions
+
+        if(resultCode == RESULT_OK) {
+            Log.d("TAG_LOGIN", "Connected");
+        }else {
+            if (loginResponse == null) {
+                Log.d("TAG_LOGIN", "Canceled");
+            }else if(loginResponse.getError()==null){
+                Log.d("TAG_LOGIN", "Unknown error");
+            }else if (loginResponse.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                Log.d("TAG_LOGIN", "No network");
+            }else if (loginResponse.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                Log.d("TAG_LOGIN", "Unknown error");
+            }else{
+                Log.d("TAG_LOGIN", "Unknown error");
+            }
+        }
     }
 }
