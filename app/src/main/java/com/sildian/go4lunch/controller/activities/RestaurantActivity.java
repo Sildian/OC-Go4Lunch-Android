4cd556c99080c673ba97ce82ec36f555d5edb76f
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2019-07-29 18:57:34 +0200

    Created 'RestaurantActivity', 'RestaurantFragment' and their views

---
+++ app/src/main/java/com/sildian/go4lunch/controller/activities/RestaurantActivity.java
@@ -0,0 +1,15 @@
+package com.sildian.go4lunch.controller.activities;
+
+
+import android.os.Bundle;
+
+import com.sildian.go4lunch.R;
+
+public class RestaurantActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+    }
+}
---
+++ app/src/main/java/com/sildian/go4lunch/controller/fragments/RestaurantFragment.java
@@ -0,0 +1,29 @@
+package com.sildian.go4lunch.controller.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sildian.go4lunch.R;
+
+public class RestaurantFragment extends Fragment {
+
+
+    public RestaurantFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_restaurant, container, false);
+    }
+
+}
